2ecaca3cbec3524ad7a2a9f90a20127ece1ec488
hseong3243 <hseong3243@gmail.com>
2023-07-20 19:33:33 +0900

    fix: `ShowDto` `dscription` 필드 추가

--- src/main/java/com/programmers/ticketing/dto/show/ShowDto.java
+++ src/main/java/com/programmers/ticketing/dto/show/ShowDto.java
@@ -3,23 +3,23 @@ package com.programmers.ticketing.dto.show;
 import com.programmers.ticketing.domain.Show;
 import com.programmers.ticketing.domain.ShowType;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalTime;
 
-@Setter
 @Getter
 public class ShowDto {
     private Long showId;
     private String title;
     private ShowType showType;
     private LocalTime playtime;
+    private String description;
 
-    private ShowDto(Long showId, String title, ShowType showType, LocalTime playtime) {
+    private ShowDto(Long showId, String title, ShowType showType, LocalTime playtime, String description) {
         this.showId = showId;
         this.title = title;
         this.showType = showType;
         this.playtime = playtime;
+        this.description = description;
     }
 
     public static ShowDto from(Show show) {
@@ -27,7 +27,8 @@ public class ShowDto {
                 show.getShowId(),
                 show.getTitle(),
                 show.getShowType(),
-                show.getPlaytime()
+                show.getPlaytime(),
+                show.getDescription()
         );
     }
 }
